d32bbef3fb0fd9796ac41b47461aca8ac5b98555
Haseeb <haseebkhan2561@gmail.com>
2020-06-10 21:29:25 -0400

    Changed

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,12 +1,10 @@
 import React, { Fragment, useState } from "react";
 //Components Import Start
-import path from "path";
-
-import Display from path(__dirname + "src/components/products/display/Display");
-import Sidebar from path(__dirname + "src/components/products/sidebar/Sidebar");
-import Nav from path(__dirname + "src/components/products/navbar/Nav");
-import Main_Nav from path(__dirname + "src/components/nav/Nav");
-import Header from path(__dirname + "src/components/slideshow/Slideshow");
+import Display from "./display/Display";
+import Sidebar from "./sidebar/Sidebar";
+import Nav from "./navbar/Nav";
+import Main_Nav from "../nav/Nav";
+import Header from "../slideshow/Slideshow";
 import axios from "axios";
 
 //Components Import End
